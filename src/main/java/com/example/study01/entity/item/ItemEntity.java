ccce6e17f8afc7f0039dd7ca2ffd933b9b96c2c2
YuYoHan <zxzz8014@naver.com>
2023-09-12 22:52:40 +0900

    [Study] shopping

--- src/main/java/com/example/study01/entity/item/ItemEntity.java
+++ src/main/java/com/example/study01/entity/item/ItemEntity.java
@@ -18,9 +18,19 @@ public class ItemEntity {
     @Column(name = "item_id")
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String itemName;
+
+    @Column(nullable = false)
     private int price;
+
+    @Column(nullable = false, name = "number")
     private int stockNumber;
+
+    // 상품 상세정보를 보면 동영상이나 이미지가 있는 경우가 있는데
+    // 그럴경우 큰 데이터를 다루기 위해서 @Lob을 넣어준다.
+    @Lob
+    @Column(nullable = false)
     private String itemDetail;
     private LocalDateTime regTime;
     private LocalDateTime updateTime;
