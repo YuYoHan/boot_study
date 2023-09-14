b152b3d7f1f477e576d0aa6bf58c248288e2c24e
YuYoHan <zxzz8014@naver.com>
2023-09-14 23:24:21 +0900

    [Study] thymeleaf

--- src/main/java/com/example/study01/controller/ThymeleafController.java
+++ src/main/java/com/example/study01/controller/ThymeleafController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ThymeleafController {
     @GetMapping("/thymeleaf/ex1")
     public String ex1(Model model) {
-        model.addAttribute("data", "시작합니다.");
+        model.addAttribute("data", "스프링 부트 시작합니다.");
         return "thymeleaf/ex1";
     }
 }
