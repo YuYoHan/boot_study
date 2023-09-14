842b162934d647d585641908be6c45b22efd4355
YuYoHan <zxzz8014@naver.com>
2023-09-14 22:57:05 +0900

    [Study] thymeleaf

--- src/main/java/com/example/study01/controller/ThymeleafController.java
+++ src/main/java/com/example/study01/controller/ThymeleafController.java
@@ -1,12 +1,14 @@
 package com.example.study01.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ThymeleafController {
     @GetMapping("/thymeleaf/ex1")
-    public String ex1() {
+    public String ex1(Model model) {
+        model.addAttribute("data", "시작합니다.");
         return "thymeleaf/ex1";
     }
 }
