869a0150522921469b2b28605efc9bbb4aa76ec0
YuYoHan <zxzz8014@naver.com>
2023-09-15 14:38:34 +0900

    [Stuy] thymeleaf 공부

--- src/main/java/com/example/study01/controller/ThymeleafController.java
+++ src/main/java/com/example/study01/controller/ThymeleafController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 // 타임리프 컨트롤러
 @Controller
@@ -33,5 +35,26 @@ public class ThymeleafController {
 
         model.addAttribute("data", itemDTO);
         return "thymeleaf/ex2";
+    }
+
+    @GetMapping("/ex3")
+    public String ex3(Model model) {
+
+        List<ItemDTO> itemDTOList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            ItemDTO itemDTO = ItemDTO.builder()
+                    .itemDetail("상품 상세 설명" + i)
+                    .itemName("상품명" + i)
+                    .price(10000 + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .stockNumber(100 - i)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+            itemDTOList.add(itemDTO);
+        }
+        model.addAttribute("itemDTOList", itemDTOList);
+        return "thymeleaf/ex3";
     }
 }
